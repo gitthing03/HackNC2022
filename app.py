b679dabd692245a8cd829bd998a84338d47bdcd5
gitthing03 <86090190+gitthing03@users.noreply.github.com>
2022-11-05 15:56:06 -0400

    Basic home page

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/routing")
+def route():
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
