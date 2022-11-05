2dc766d9383a6be15554aad8d196512cd12db3e1
Amanda Lim <54029190+amandalim857@users.noreply.github.com>
2022-11-05 17:53:54 -0400

    Add concerns template

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/concerns")
+def concerns():
+    return render_template("concerns.html")
+    # item: items)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 if __name__ == "__main__":
     app.run(debug=True)
 
