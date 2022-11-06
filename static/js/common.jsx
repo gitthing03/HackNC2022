82a4fa2e173702ac6bffe3e625a07f6277aa10bd
jadenPete <jadenpete@github.com>
2022-11-05 23:02:12 -0400

    Integrated the OpenStreetMap API with the Directions page

---
+++ static/js/common.jsx
@@ -0,0 +1,108 @@
+const DEFAULT_CENTER = ol.proj.fromLonLat([-79.046761, 35.904613])
+
+const STROKE_COLOR = "#800080"
+
+function Map() {
+	const popupContainerRef = React.useRef()
+
+	const [popupContent, setPopupContent] = React.useState("")
+
+	React.useEffect(() => {
+		const attribution = new ol.control.Attribution({
+			collapsible: false
+		});
+
+		const map = new ol.Map({
+			controls: ol.control.defaults({attribution: false}).extend([attribution]),
+			layers: [
+				new ol.layer.Tile({
+					source: new ol.source.OSM({
+						url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+						attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://openstreetmap.org/">OSM</a>' ],
+						maxZoom: 20,
+						minZoom: 16
+					})
+				})
+			],
+			target: "map",
+			view: new ol.View({
+				center: DEFAULT_CENTER,
+				maxZoom: 20,
+				zoom: 17,
+				minZoom: 16
+			})
+		});
+
+
+		const layer = new ol.layer.Vector({
+			  source: new ol.source.Vector({
+				  features: [
+					  new ol.Feature({
+						  geometry: new ol.geom.Point(DEFAULT_CENTER)
+					  })
+				  ]
+			  })
+		});
+
+		map.addLayer(layer);
+
+		const overlay = new ol.Overlay({
+			element: popupContainerRef.current,
+			autoPan: true,
+			autoPanAnimation: {
+				duration: 250
+			}
+		});
+
+		map.addOverlay(overlay);
+		map.on("singleclick", event => {
+			if (map.hasFeatureAtPixel(event.pixel) === true) {
+				overlay.setPosition(event.coordinate)
+
+				setPopupContent("<b>Hello world!</b><br/>I am a popup.")
+			} else {
+			  overlay.setPosition(undefined);
+			}
+	  	});
+
+		// https://stackoverflow.com/a/52244847
+
+		var coordinates = [[-79.045869, 35.909347], [-79.0475, 35.91]];
+
+		var lineString = new ol.geom.LineString(coordinates);
+
+		lineString.transform("EPSG:4326", "EPSG:3857");
+
+		map.addLayer(
+			new ol.layer.Vector({
+				source: new ol.source.Vector({
+					features: [
+						new ol.Feature({
+							geometry: lineString,
+							name: "Line"
+						})
+					]
+				}),
+
+				style: [
+					new ol.style.Style({
+						stroke: new ol.style.Stroke({
+							color: STROKE_COLOR,
+							width: 6
+						})
+					})
+				]
+			})
+		);
+	}, [])
+
+	return <>
+		<div id="map" class="w-100 h-100"></div>
+
+		<div class="ol-popup" ref={popupContainerRef}>
+			<div dangerouslySetInnerHTML={{
+				__html: popupContent
+			}}></div>
+		</div>
+	</>
+}
--- static/js/directions.jsx
+++ static/js/directions.jsx
@@ -36,7 +36,7 @@ function Main() {
 			<div class="d-flex flex-grow-1">
 				<div class="card col-9 bg-light border me-3 shadow">
 					<div class="card-body">
-						Map
+						<Map/>
 					</div>
 				</div>
 
@@ -50,112 +50,4 @@ function Main() {
 	)
 }
 
-function robertsStuff() {
-	var attribution = new ol.control.Attribution({
-		collapsible: false
-	});
-
-	var map = new ol.Map({
-		controls: ol.control.defaults({attribution: false}).extend([attribution]),
-		layers: [
-			new ol.layer.Tile({
-				source: new ol.source.OSM({
-					url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-					attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://openstreetmap.org/">OSM</a>' ],
-					maxZoom: 20,
-					minZoom: 16
-				})
-			})
-		],
-		target: 'map',
-		view: new ol.View({
-		  //-79.046761, 35.904613
-			center: ol.proj.fromLonLat([-79.046761, 35.904613]),
-			maxZoom: 20,
-			zoom: 17,
-			minZoom: 16
-		})
-	});
-
-
-	var layer = new ol.layer.Vector({
-	  source: new ol.source.Vector({
-		  features: [
-			  new ol.Feature({
-				  geometry: new ol.geom.Point(ol.proj.fromLonLat([-79.046761, 35.904613]))
-			  })
-		  ]
-	  })
-	});
-	map.addLayer(layer);
-
-	var container = document.getElementById('popup');
-	var content = document.getElementById('popup-content');
-	var closer = document.getElementById('popup-closer');
-
-	var overlay = new ol.Overlay({
-		element: container,
-		autoPan: true,
-		autoPanAnimation: {
-			duration: 250
-		}
-	});
-	map.addOverlay(overlay);
-
-	closer.onclick = function() {
-		overlay.setPosition(undefined);
-		closer.blur();
-		return false;
-	};
-
-	map.on('singleclick', function (event) {
-	  if (map.hasFeatureAtPixel(event.pixel) === true) {
-		  var coordinate = event.coordinate;
-
-		  content.innerHTML = '<b>Hello world!</b><br />I am a popup.';
-		  overlay.setPosition(coordinate);
-	  } else {
-		  overlay.setPosition(undefined);
-		  closer.blur();
-	  }
-	});
-
-
-	// LINE REFERENCE: https://stackoverflow.com/a/52244847
-
-	var coordinates = [[-79.045869, 35.909347], [-79.0475, 35.91]];
-
-	var lineString = new ol.geom.LineString(coordinates);
-	lineString.transform('EPSG:4326', 'EPSG:3857');
-
-	var feature = new ol.Feature({
-	  geometry: lineString,
-	  name: 'Line'
-	});
-
-	var lineStyle = new ol.style.Style({
-	  stroke: new ol.style.Stroke({
-		color: '#800080',
-		width: 6
-	  })
-	});
-
-	var source = new ol.source.Vector({
-	  features: [feature]
-	});
-	var vector = new ol.layer.Vector({
-	  source: source,
-	  style: [lineStyle]
-	});
-	map.addLayer(vector);
-}
-
 ReactDOM.render(<Main/>, document.querySelector(".wrapper"))
-
-<div id="map" style="width: 800px; height: 700px;"></div>
-<div id="popup" class="ol-popup">
-  <a href="#" id="popup-closer" class="ol-popup-closer"></a>
-  <div id="popup-content"></div>
-</div>
-
-robertsStuff()
